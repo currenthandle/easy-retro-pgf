5dd4dac5fc2318ea6d7857d73fc0b62b9589d28e
Casey <leptone55@gmail.com>
2024-04-18 16:13:41 -0700

    sts

--- src/server/api/routers/ballot.ts
+++ src/server/api/routers/ballot.ts
@@ -10,10 +10,46 @@ import {
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 import { ballotTypedData } from "~/utils/typedData";
 import type { db } from "~/server/db";
-import { config } from "~/config";
+import { config, eas } from "~/config";
 import { sumBallot } from "~/features/ballot/hooks/useBallot";
 import { type Prisma } from "@prisma/client";
 import { fetchApprovedVoter } from "~/utils/fetchAttestations";
+import engine from "~/ezkl/engine";
+import fs from "fs";
+import path from "path";
+import { fetchAttestations, createDataFilter } from "~/utils/fetchAttestations";
+
+// console.log('path', __dirname);
+// const vk = fs.readFileSync("~/src/ezkl/artifacts/vk.key");
+//
+// console.log("vk", vk);
+// const vkPath = path.join(__dirname, "../../../../src/ezkl/artifacts/vk.key");
+const rootDir = path.resolve(__dirname, "../../../../../"); // Adjust this as necessary
+const vkPath = path.join(rootDir, "src/ezkl/artifacts/vk.key");
+const settingsPath = path.join(rootDir, "src/ezkl/artifacts/settings.json");
+const srsPath = path.join(rootDir, "src/ezkl/artifacts/kzg25.srs");
+
+let vk: Uint8ClampedArray;
+let settings: Uint8ClampedArray;
+let srs: Uint8ClampedArray;
+
+try {
+  const vkBuf = fs.readFileSync(vkPath);
+  const settingsBuf = fs.readFileSync(settingsPath);
+  const srsBuf = fs.readFileSync(srsPath);
+
+  vk = new Uint8ClampedArray(vkBuf);
+  settings = new Uint8ClampedArray(settingsBuf);
+  srs = new Uint8ClampedArray(srsBuf);
+
+  console.log("VK loaded successfully");
+  console.log("VK", vk);
+  console.log("Settings", settings);
+  console.log("SRS", srs);
+} catch (error) {
+  console.log("VK Path", vkPath);
+  console.error("Error loading the VK:", error);
+}
 
 const defaultBallotSelect = {
   votes: true,
@@ -43,12 +79,50 @@ export const ballotRouter = createTRPCRouter({
       }
       await verifyUnpublishedBallot(voterId, ctx.db);
 
+      const projectCount = await countApprovedProjects();
+
+      // const kzgCommitment = createKZGCommitment(input.votes);
+      const kzgCommitment = createKZGCommitment();
+
+      console.log("==================================================");
+      console.log("engine", engine);
+      console.log("input", input);
+
       return ctx.db.ballot.upsert({
         select: defaultBallotSelect,
         where: { voterId },
-        update: input,
-        create: { voterId, ...input },
+        update: { ...input, kzgCommitment },
+        create: { voterId, ...input, kzgCommitment },
       });
+
+      function createKZGCommitment() {
+        const message: Uint8ClampedArray = new Uint8ClampedArray(32);
+        const commitment = engine.kzgCommit(message, vk, settings, srs);
+        const commitmentStr = new TextDecoder().decode(commitment);
+        return commitment;
+      }
+
+      async function countApprovedProjects() {
+        return fetchAttestations([eas.schemas.approval], {
+          where: {
+            attester: { in: config.admins },
+            AND: [
+              createDataFilter("type", "bytes32", "application"),
+              createDataFilter("round", "bytes32", config.roundId),
+            ],
+          },
+        }).then((attestations = []) => {
+          // Handle multiple approvals of an application - group by refUID
+          return {
+            count: Object.keys(
+              attestations.reduce(
+                (acc, x) => ({ ...acc, [x.refUID]: true }),
+                {},
+              ),
+            ).length,
+          };
+        });
+      }
     }),
   publish: protectedProcedure
     .input(BallotPublishSchema)
